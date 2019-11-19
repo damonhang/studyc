b10d1a8eeb94bd86970e4392d7e41618e2478767
yuanhang.xie <405877095@qq.com>
2019-11-19 19:21:26 +0800

    增加贪吃蛇程序

---
+++ mainTest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "modules/function.h"
+
+int main()
+{
+    int a=2,b=3;
+    int c=add(a,b);
+    printf("µËÀ×SB \n");
+    getchar();
+    return 0;
+}
